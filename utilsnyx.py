e23d1efc42e34c6a30314b52d226b7d0e91f3fc1
Cappycot <chriswang444@gmail.com>
2017-03-22 15:42:57 -0500

    Fixed up binary search

--- utilsnyx.py
+++ utilsnyx.py
@@ -4,25 +4,32 @@ def binary_search(array, query, key = lambda a: a, start = 0, end = -1):
     """Python's 'in' keyword performs a linear search on arrays.
     Given the circumstances of storing sorted arrays, it's better
     for Nyx to use a binary search.
-    A lambda key can be specified for member variables or whatever.
+    
+    Keyword Arguments:
+    key - filter for objects in the array
+    start - 0-indexed starting marker on the array to search
+    end - exclusive ending marker on the array to search
     """
     if array is None or len(array) == 0:
         return None
-    elif len(array) == 1:
-        return key(array[0]) == query and array[0] or None
+    # elif len(array) == 1:
+        # return key(array[0]) == query and array[0] or None
     
     if end == -1:
         end = len(array)
     elif start >= end:
         return None
-    mid = int(len(array) / 2)
     
-    mid = int(len(array) / 2)
+    print(start, "and", end)
+    mid = int(start + (end - start) / 2)
+    print(mid)
+    
+    # mid = int(len(array) / 2)
     compare_to = key(array[mid]) # Applies lambda to array items.
     if query < compare_to:
-        return binary_search(array[:mid], query, key)
+        return binary_search(array, query, key, start, mid)
     elif query > compare_to:
-        return binary_search(array[mid + 1:], query, key)
+        return binary_search(array, query, key, mid + 1, end)
     else:
         return array[mid]
 
