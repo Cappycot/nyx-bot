21d2391855bd2c06bf4975ce05b3e3b4eec3cd1d
Cappycot <chriswang444@gmail.com>
2017-03-30 11:27:23 -0500

    Greatly improved photo editing quality and speed because of masking

--- modules/tests/modtests.py
+++ modules/tests/modtests.py
@@ -17,6 +17,50 @@ testdir = None
 # Command Functions
 ###############################################################################
 
+async def fear(client = None, message = None, **_):
+    """Concept test of PIL usage and image editing..."""
+    
+    # Check to make sure everything is there.
+    if client is None or message is None:
+        return "Something disappeared... idk."
+    elif len(message.mentions) > 1 or message.server is None:
+        return "You have too many mentions..."
+    
+    # Get user to post
+    tofear = message.mentions[0] if len(message.mentions) == 1 else message.author
+    
+    global testdir
+    try:
+        await client.send_typing(message.channel)
+        url = tofear.avatar_url
+        if not url:
+            url = tofear.default_avatar_url
+        
+        # Presets for image creation
+        avatarside = 240
+        fpos = (522, 132)
+        
+        # Make http request for profile picture and get background
+        tofear = Image.open(io.BytesIO(http.request("GET", url).data)).convert("RGBA")
+        backdrop = Image.open(testdir + "/NoFearOneFear.png")
+        
+        # Paste avatar pic on top of backdrop as resized image
+        tofear = tofear.resize((avatarside, avatarside), Image.LANCZOS)
+        backdrop.paste(tofear, (fpos[0], fpos[1], fpos[0] + avatarside, fpos[1] + avatarside), mask=tofear)
+        
+        # Save in-memory filestream and send to Discord
+        testpic = io.BytesIO()
+        backdrop.save(testpic, format="png")
+        # Move pointer to beginning so Discord can read pic.
+        testpic.seek(0) 
+        await client.send_file(message.channel, testpic, filename = "nofearonefear.png")
+    except:
+        err = exc_info()
+        for e in err:
+            print(e)
+        return "Uhh... something went wrong..."
+
+
 async def obliterate(client = None, message = None, **_):
     """Concept test of PIL usage and image editing..."""
     
@@ -51,53 +95,43 @@ async def obliterate(client = None, message = None, **_):
         vpos = (260, 360)
         
         # Make http request for profile pictures and get background tank
-        shooter = Image.open(io.BytesIO(http.request("GET", url1).data))
-        victim = Image.open(io.BytesIO(http.request("GET", url2).data))
+        shooter = Image.open(io.BytesIO(http.request("GET", url1).data)).convert("RGBA")
+        victim = Image.open(io.BytesIO(http.request("GET", url2).data)).convert("RGBA")
         backdrop = Image.open(testdir + "/Obliterate.png")
         
         # Paste shooter on top of tank as resized image
         shooter = shooter.resize((avatarside, avatarside), Image.LANCZOS)
-        vback = Image.new("RGB", (512, 512), (0, 0, 0))
-        vback.paste(shooter, (spos[0], spos[1],
-                    spos[0] + avatarside, spos[1] + avatarside))
-        shooter = vback.getdata()
-        backdata = backdrop.getdata()
-        mixdata = []
-        for i in range(0, len(shooter)):
-            item = shooter[i]
-            if item[0] + item[1] + item[2] == 0:
-                mixdata.append(backdata[i])
-            else:
-                mixdata.append(item)
+        backdrop.paste(shooter, (spos[0], spos[1], spos[0] + avatarside,
+                                spos[1] + avatarside), mask=shooter)
                 
         # backdrop.paste(shooter, (spos[0], spos[1],
         #                spos[0] + avatarside, spos[1] + avatarside))
         
-        # Rotate victim and clear out black noise
+        # Resize, tint, rotate, and paste victim
         victim = victim.resize((avatarside, avatarside), Image.LANCZOS)
+        
+        data = victim.getdata()
+        new_data = []
+        for i in range(0, len(data)):
+            item = data[i]
+            # Tint red and brighten a bit
+            red = min(item[0] + 42, 255)
+            green = min(item[1] + 9, 255)
+            blue = max(item[2] - 9, 0)
+            new_data.append((red, green, blue, item[3]))
+        victim.putdata(new_data)
+        
         victim = victim.rotate(-40, expand = True)
         vw = victim.size[0]
         vh = victim.size[1]
-        vback = Image.new("RGB", (512, 512), (0, 0, 0))
-        vback.paste(victim, (vpos[0], vpos[1], vpos[0] + vw, vpos[1] + vh))
-        victim = vback.getdata()
-        backdata = mixdata
-        mixdata = []
-        for i in range(0, len(victim)):
-            item = victim[i]
-            if item[0] + item[1] + item[2] == 0:
-                mixdata.append(backdata[i])
-            else:
-                red = min(item[0] + 42, 255)
-                green = min(item[1] + 9, 255)
-                blue = max(item[2] - 9, 0)
-                mixdata.append((red, green, blue))
+        backdrop.paste(victim, (vpos[0], vpos[1], vpos[0] + vw,
+                                vpos[1] + vh), mask=victim)
         
-        # Put it all together
-        backdrop.putdata(mixdata)
+        # Save in-memory filestream and send to Discord
         testpic = io.BytesIO()
-        backdrop.save(testpic, format="png") # Save bytes to stream.
-        testpic.seek(0) # Move pointer to beginning so Discord can read pic.
+        backdrop.save(testpic, format="png")
+        # Move pointer to beginning so Discord can read pic.
+        testpic.seek(0)
         await client.send_file(message.channel, testpic, filename = "bwaarp.png")
         return "Another one bites the dust! ``*CLAP*``"
     except:
@@ -107,7 +141,8 @@ async def obliterate(client = None, message = None, **_):
         return "Uhh... we misfired, sir!"
 
 
-commands = [[["obliterate", "destroy", "pwn"], obliterate, "Reks a user.", "obliterate @user", -1]]
+commands = [[["fear"], fear, "When you or someone else is scary.", "fear [@user]", -1],
+            [["obliterate", "destroy", "pwn"], obliterate, "Reks a user.", "obliterate @user", -1]]
 
 
 def init(module = None, **_):
