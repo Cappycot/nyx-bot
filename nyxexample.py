0bb60c1f6fde0222c0a691d584477bcd7fbeb9b9
Chris Wang <chriswang444@gmail.com>
2018-03-21 23:00:05 -0500

    Add example documentation

--- nyxexample.py
+++ nyxexample.py
@@ -1,11 +1,17 @@
+"""
+This is a simple file that can run Nyx. It will take the token from the
+info.nyx (or whatever config) file and use it to start your Discord bot up.
+The name of the folder where cogs are stored can be specified here as well.
+"""
+
 from configparser import ConfigParser
 from nyx import Nyx
 
-nyx_cog_folder = "cogs"
-nyx_config_file = "info.nyx"
+nyx_cog_folder = "cogs"  # The folder where cogs will be searched for.
+nyx_config_file = "info.nyx"  # The name of the config file used.
 
 nyx = Nyx()
-nyx.load_cogs(nyx_cog_folder)
+nyx.load_cogs(nyx_cog_folder)  # Get cogs from specified folder.
 
 nyx_config = ConfigParser()
 nyx_config.read(nyx_config_file)
