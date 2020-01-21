d291983ac87fd1259a7c36ab8e5f8b2fbaf5c09c
Chris Wang <chriswang444@gmail.com>
2020-01-20 23:39:29 -0600

    Add nyxguild and nyxuser

--- nyx/__init__.py
+++ nyx/__init__.py
@@ -1,4 +1,6 @@
 from .nyxbot import NyxBot
 from .nyxcommands import *
+from .nyxguild import NyxGuild
 from .nyxhelp import DefaultNyxHelpCommand
 from .nyxsplash import splash
+from .nyxuser import NyxUser
